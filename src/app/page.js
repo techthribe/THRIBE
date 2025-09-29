f860b3859f59005bb6214228b3617cffbb11742a
osuji chiaka <chiakajunior@yahoo.com>
2025-09-29 18:04:03 +0100

    working

--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,7 @@ import JoinCommunity from "./components/joinCommunity";
 import Testimaonial from "./components/testimonials";
 import Collaborate from "./components/collaborate";
 import Footer from "./components/Footer";
+import Link from "next/link";
 
 
 export default function Home() {
@@ -124,10 +125,12 @@ export default function Home() {
          </div>
 
     <div className="mt-[32px] flex justify-center">
+      <Link href="#joinCommunity">
      <button className={`flex items-center space-x-[5px] justify-center bg-[#107269] w-[244px] md:w-[284px] h-[47px] md:h-[56px] cursor-pointer shadow-[4px_4px_0px_0px_#003E39] hover:shadow-[4px_4px_0px_0px_#64C0B7] font-[500] text-[18px] text-[#fff] rounded-[100px]`}>
         <span>Join The community</span>
         <Image src="/img/arrow-right.png" width={24} height={24} alt="thribe community" className="" />
      </button>
+     </Link>
      </div>  
       </div>
 
@@ -205,7 +208,9 @@ export default function Home() {
 
         <p className="mt-[24px]">This is your THR!BE - The tribe that thrives.</p>
         </div>
+        <Link href="/#joinCommunity">
         <Button name="Join The tribe Now" classname="w-[196px] md:w-[236px] h-[47px] md:h-[56px] bg-primaryColor" />
+        </Link>
       </div>
 
       <div className="w-full xl:w-[600px]">
@@ -252,7 +257,9 @@ export default function Home() {
            not hustle culture. Tech isn&#39;t just about work - it&#39;s about people. At THR!BE, you&#39;ll find a tribe that gets it. 
            We mix learning with laughter, growth with community, and celebration with impact. We laugh, play, grow, and thrive - together. 
           </p>
+          <Link href="/#joinCommunity">
           <Button name="Join THR!BE" classname="bg-[#B15301] w-[144px] md:w-[184px] h-[47px] md:h-[56px] shadow-[4px_4px_0px_0px_#693101] hover:shadow-[4px_4px_0px_0px_#FBA355]" />
+          </Link>
         </div>
         </div>
       </div>
