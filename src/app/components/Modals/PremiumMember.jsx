b6de4d95328cf3553e951b395b85b27e0203dbe9
osuji chiaka <chiakajunior@yahoo.com>
2025-09-30 22:21:14 +0100

    working

--- src/app/components/Modals/PremiumMember.jsx
+++ src/app/components/Modals/PremiumMember.jsx
@@ -1,12 +1,31 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAllContext } from "../../context/allcontext";
 import Image from "next/image";
 import Button from "../Button";
 
 export default function Modal({ }) {
     const {premiumMemberModal, togglePremiumMemberModal} = useAllContext();
+    const [formDatas, setFormDatas] = useState({
+        fullname: "",
+        role: "",
+        email: "",
+        phone_no: "",
+        payment_plan: "",
+        why_join_thribe: "",
+        accept_terms: false
+    })
 
+    const onChangeFormDataFunctions = (e) => {
+      const { name, type, value, checked } = e.target;
+
+
+        setFormDatas((prev) => ({
+            ...prev,
+            [name]: type === "checkbox" ? checked :  value
+        }))
+    }
+ 
   // prevent background scroll when modal is open
   useEffect(() => {
     if (premiumMemberModal) {
@@ -27,7 +46,7 @@ export default function Modal({ }) {
       ></div>
 
       {/* modal content */}
-      <div className="overflow-y-scroll tracking-[2%] leading-[150%] relative bg-[#E8F6F4] rounded-lg shadow-lg z-10 w-[95%] lg:w-[1200px] 2xl:w-[1404px] max-h-[90vh] rounded-[16px] py-[40px] xl:py-[120px] px-[16px] xl:px-[90px] mt-[20px]">
+      <div className="overflow-y-auto tracking-[2%] leading-[150%] relative bg-[#E8F6F4] rounded-lg shadow-lg z-10 w-[95%] lg:w-[1200px] 2xl:w-[1404px] max-h-[90vh] rounded-[16px] py-[40px] xl:py-[120px] px-[16px] xl:px-[90px] mt-[20px]">
         <h2 className="text-[16px] tracking-[16%] text-primaryGreen">THR!BE WITH US FOR FREE</h2>
          <h1 className="mt-[16px] flex items-center gap-x-[12px] font-clash font-[600] text-[26px] md:text-[46px] leading-[100%] tracking-[3%] text-primaryTextColor">
             <span>Be a Premium Member</span>
@@ -40,8 +59,16 @@ export default function Modal({ }) {
              {/*form for mobile view */}
             <div className="leading-[150%] tracking-[3%] w-full mt-[24px] md:mt-[48px] flex gap-x-[24px] gap-y-[24px] md:gap-y-[40px] flex-wrap items-end block md:hidden">
                     <div className="flex flex-col md:flex-row gap-y-[16px] md:gap-y-0 md:gap-x-[24px]">
-                    <label htmlFor="names" className="">My name is</label>
-                    <input id="names" type="text" placeholder="Enter your full name"  className="w-[278px] placeholder-[#87857F] text-[16px] md:text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/>
+                    <label htmlFor="fullname" className="">My name is</label>
+                    <input 
+                    name="fullname"
+                    value={formDatas.fullname}
+                    onChange={onChangeFormDataFunctions}
+                    id="fullname" 
+                    type="text" 
+                    placeholder="Enter your full name"  
+                    className="w-[278px] placeholder-[#87857F] text-[16px] md:text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                    />
                     </div>
                     <div className="flex flex-col md:flex-row gap-y-[16px] md:gap-y-0 md:gap-x-[24px] w-full md:w-[542px]">
                     <label htmlFor="job" className="">and I am a </label>
@@ -79,22 +106,98 @@ export default function Modal({ }) {
 
             {/* form for webview */}
              <div className="tracking-[3%] w-full hidden md:flex gap-x-[12px] gap-y-[40px] flex-wrap items-end ">
-                    <label htmlFor="names" className="">My name is</label>
-                    <input id="names" type="text" placeholder="Enter your full name"  className="w-[278px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/>
+                    <label htmlFor="fullname" className="">My name is</label>
+                    <input 
+                    id="fullname" 
+                    name="fullname"
+                    value={formDatas.fullname}
+                    onChange={onChangeFormDataFunctions}
+                    type="text" 
+                    placeholder="Enter your full name"  
+                    className="w-[278px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                    />
                     <label htmlFor="role" className="">and I am a </label>
-                    <div><input id="role" type="text" placeholder="What field are you in? (developer, designer...)"  className="w-[384px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/> .</div>
-                    <label htmlFor="address" className="">My email address is</label>
-                    <div><input id="address" type="text" placeholder="Enter your email address?"  className="w-[396px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/> .</div>
+                    <div className="relative w-[433px]">
+                         <Image src="/icons/arrow-bottom-black.png" width={20} height={20} alt="thribe community" className="object-fit absolute bottom-0 right-0" />
+                        <select id="role" 
+                            name="role"
+                            value={formDatas.role}
+                            onChange={onChangeFormDataFunctions}
+                            className="appearance-none cursor-pointer bg-[#E8F6F4] w-[433px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                         >
+                            <option vlaue="">What field are you in? (developer, designer...)</option>
+                            <option value="Product Designer" className="hover:bg-[#fff] cursor-pointer">Product Designer</option>
+                            <option value="Product MAanager" className="hover:bg-[#fff] cursor-pointer">Product Manager</option>
+                            <option value="Unit Tester" className="hover:bg-[#fff] cursor-pointer">Unit Tester</option>
+                            <option value="Frontend Developer" className="hover:bg-[#fff] cursor-pointer">Frontend Developer</option>
+                            <option value="Backend Developer" className="hover:bg-[#fff] cursor-pointer">Backend Developer</option>
+                            <option value="Graphic Designer" className="hover:bg-[#fff] cursor-pointer">Graphic Designer</option>
+                        </select>
+                    </div>
+                    <label htmlFor="email" className="">My email address is</label>
+                    <div>
+                        <input 
+                        name="email"
+                        value={formDatas.email}
+                        onChange={onChangeFormDataFunctions}
+                        id="email" 
+                        type="email" 
+                        placeholder="Enter your email address?"  
+                        className="w-[396px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                        /> 
+                        .
+                        </div>
                      <label htmlFor="phone" className="">My phone number  is </label>
-                    <div><input id="phone" type="text" placeholder="Enter your phone number"  className="w-[296px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/> .</div>
+                    <div>
+                        <input 
+                         name="phone_no"
+                        value={formDatas.phone_no}
+                        onChange={onChangeFormDataFunctions}
+                        id="phone" 
+                        type="text" 
+                        placeholder="Enter your phone number"  
+                        className="w-[296px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                        /> 
+                        .
+                    </div>
                     <label htmlFor="payment" className="">My Preferred payment plan is</label>
-                    <div><input id="payment" type="text" placeholder="Enter your phone number"  className="w-[396px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/> .</div>
-                    <label htmlFor="why_join_thribe" className="">I will love to join THRIBE because </label>
-                    <div><input id="why_join_thribe" type="text" placeholder="Tell us why you would love to join THR!BE"  className="w-[502px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"/></div>
+                     <div className="relative w-[433px]">
+                         <Image src="/icons/arrow-bottom-black.png" width={20} height={20} alt="thribe community" className="object-fit absolute bottom-0 right-0" />
+                        <select
+                        name="payment_plan"
+                        value={formDatas.payment_plan}
+                        onChange={onChangeFormDataFunctions}
+                         id="payment"  
+                         className="appearance-none cursor-pointer bg-[#E8F6F4] w-[433px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                         >
+                            <option value="">Select your preferred plan</option>
+                            <option value="monthly" className="cursor-pointer">Monthly Plan</option>
+                            <option value="quarterly" className="cursor-pointer">Quarterly Plan</option>
+                            <option value="yearly" className="cursor-pointer">Yearly Plan</option>
+                           
+                        </select>
+                    </div>
+                     <label htmlFor="why_join_thribe" className="">I will love to join THRIBE because </label>
+                    <div>
+                    <input
+                     name="why_join_thribe"
+                    value={formDatas.why_join_thribe}
+                    onChange={onChangeFormDataFunctions}
+                     id="why_join_thribe" 
+                     type="text" 
+                     placeholder="Tell us why you would love to join THR!BE"  
+                     className="w-[462px] placeholder-[#87857F] text-[18px]  border border-x-0 border-t-0  border-b-[#0A1A18] inline px-[5px] outline-none"
+                     /></div>
             </div>
 
             <div className="w-full flex space-x-[16px] mt-[24px] md:mt-[40px]">
-                <input type="checkbox"  className="border-[#B3B9C4] rounded-[4px] bg-[#fff]" />
+                <input
+                 name="accept_terms"
+                 checked={formDatas.accept_terms}
+                 onChange={onChangeFormDataFunctions}
+                 type="checkbox"  
+                 className="border-[#B3B9C4] rounded-[4px] bg-[#fff]" 
+                 />
                 <span className="text-secondaryText">I have read and agree to the <span className="text-primaryGreen font-[500]">terms & conditions</span></span>
             </div>
 
